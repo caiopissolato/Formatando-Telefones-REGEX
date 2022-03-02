72451f713c3fd1ebfbfca38e8783569c7d0225d0
Caio Pissolato <caio_pissolato.05@hotmail.com>
2022-03-02 18:38:27 -0300

    Formatando Telefones com REGEX

---
+++ main.py
@@ -0,0 +1,10 @@
+from TelefonesBr import TelefonesBr
+
+telefone = "5566999027227"
+telefone_objeto = TelefonesBr(telefone)
+
+#padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+#resposta = re.search(padrao,telefone)
+#print(resposta.group())
+
+print(telefone_objeto)
